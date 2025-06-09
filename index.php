0aeda619f2ecf27092c7da37559b4d964d492ca5
ua-Slash-ua <so.slashua@email.com>
2025-06-09 10:58:08 +0300

    Базова структура + деякі основні файли

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: SL Applications
+ * Description: Плагін для залишення заявок.
+ * Version: 1.0
+ * Author: ua-Slash-ua
+ */
+
+// Захист від прямого доступу
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+define("SL_APPLICATIONS_PATH", plugin_dir_path( __FILE__ ));
+define("SL_APPLICATIONS_URL", plugin_dir_url( __FILE__ ));
+
+include_once SL_APPLICATIONS_PATH . "fincludes.php";
+function my_applications_activate() {
+    error_log('My Applications Plugin активовано!');
+}
+register_activation_hook( __FILE__, 'my_applications_activate' );
--- main.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Plugin Name: SL Applications
- * Description: Плагін для залишення заявок.
- * Version: 1.0
- * Author: ua-Slash-ua
- */
