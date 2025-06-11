e14fb979086b37fcbe5ddaa8bc2bfa26ce86dbf9
ua-Slash-ua <so.slashua@gmail.com>
2025-06-11 18:06:33 +0300

    Виправлено баг рекурсії і якщо data is object daat = [data]

--- assets/scripts/ap_settings_endpoint.js
+++ assets/scripts/ap_settings_endpoint.js
@@ -60,18 +60,21 @@ function getEndpointTypes(func){
 }
 function loadEndpointTypes(data) {
     try {
+        if (!Array.isArray(data)) {
+            data = [data]
+        }
         let name_data = {
-            'name':'Назва',
-            'slug':'Слаг',
+            'name': 'Назва',
+            'slug': 'Слаг',
         }
         const ulContainer = document.querySelector('.preview_types_list');
         ulContainer.innerHTML = ''; // очищаємо попередній список
 
         data.forEach(type => {
             const liContainer = document.createElement('li');
-            liContainer.classList.add('preview_types_list_item')
-            for (let k in type){
+            liContainer.classList.add('preview_types_list_item');
 
+            for (let k in type) {
                 const divItem = document.createElement('div');
 
                 const labelItem = document.createElement('span');
@@ -83,58 +86,56 @@ function loadEndpointTypes(data) {
                 divItem.appendChild(labelItem);
                 divItem.appendChild(spanItem);
                 liContainer.appendChild(divItem);
-
             }
+
             const divAction = document.createElement('div');
 
             const btnEdit = document.createElement('input');
-            btnEdit.type = 'button'
+            btnEdit.type = 'button';
             btnEdit.value = 'Edit';
-            btnEdit.addEventListener('click',function (){
-                let new_data = {}
-                const popUp = document.querySelector('.pop-up-edit-type')
-                const nameEdit = document.querySelector('#ed_at_edit_name')
-                nameEdit.value = type['name']
-                const slugEdit = document.querySelector('#ed_at_edit_slug')
-                slugEdit.value = type['slug']
-                popUp.classList.add('active')
-                const btnEditSend = document.querySelector('#ed_at_edit_type')
-                btnEditSend.addEventListener('click',function (){
-                    if (!nameEdit.value.trim() || !slugEdit.value.trim() || !isValidSlug(slugEdit.value.trim())) {
-                        alertMessage('Некоректно введені дані для type ендпоінта!', 'error')
-                    } else {
+            btnEdit.addEventListener('click', function () {
+                let new_data = {};
+                const popUp = document.querySelector('.pop-up-edit-type');
+                const nameEdit = document.querySelector('#ed_at_edit_name');
+                nameEdit.value = type['name'];
+                const slugEdit = document.querySelector('#ed_at_edit_slug');
+                slugEdit.value = type['slug'];
+                popUp.classList.add('active');
 
-                        new_data['name'] = nameEdit.value.trim()
-                        new_data['slug'] = slugEdit.value.trim()
-                        callWpAjaxFunction('edit_endpoint_type', [type, new_data ])
+                const btnEditSend = document.querySelector('#ed_at_edit_type');
+                btnEditSend.addEventListener('click', function () {
+                    if (!nameEdit.value.trim() || !slugEdit.value.trim() || !isValidSlug(slugEdit.value.trim())) {
+                        alertMessage('Некоректно введені дані для type ендпоінта!', 'error');
+                    } else {
+                        new_data['name'] = nameEdit.value.trim();
+                        new_data['slug'] = slugEdit.value.trim();
+                        callWpAjaxFunction('edit_endpoint_type', [type, new_data])
                             .then(response => {
-                                getEndpointTypes(loadEndpointTypes)
-                                alertMessage(`Тип заявки ${type['name']} < ${type['slug']} > успішно змінено!`, 'success')
+                                getEndpointTypes(loadEndpointTypes);
+                                alertMessage(`Тип заявки ${type['name']} <${type['slug']}> успішно змінено!`, 'success');
                             })
                             .catch(error => {
-                                alertMessage(`Не вдалося змінити ${type['name']} < ${type['slug']} > тип заявки!`, 'error')
+                                alertMessage(`Не вдалося змінити ${type['name']} <${type['slug']}> тип заявки!`, 'error');
                             });
-                        popUp.classList.remove('active')
-
+                        popUp.classList.remove('active');
                     }
-                })
-            })
-
+                });
+            });
 
             const btnRemove = document.createElement('input');
-            btnRemove.type = 'button'
+            btnRemove.type = 'button';
             btnRemove.value = 'X';
-            btnRemove.addEventListener('click',function (){
+            btnRemove.addEventListener('click', function () {
                 callWpAjaxFunction('remove_endpoint_type', type)
                     .then(response => {
-                        console.log(response)
-                        alertMessage(`Тип заявки ${type['name']} < ${type['slug']} > успішно видалено!`, 'success')
+                        console.log(response);
+                        alertMessage(`Тип заявки ${type['name']} <${type['slug']}> успішно видалено!`, 'success');
+                        getEndpointTypes(loadEndpointTypes); // Оновлення після видалення
                     })
                     .catch(error => {
-                        alertMessage(`Не вдалося отримати видалити ${type['name']} < ${type['slug']} > тип заявки!`, 'error')
+                        alertMessage(`Не вдалося видалити ${type['name']} <${type['slug']}> тип заявки!`, 'error');
                     });
-                getEndpointTypes(loadEndpointTypes)
-            })
+            });
 
             divAction.appendChild(btnEdit);
             divAction.appendChild(btnRemove);
@@ -142,10 +143,12 @@ function loadEndpointTypes(data) {
 
             ulContainer.appendChild(liContainer);
         });
+
     } catch (error) {
         console.error('Помилка при завантаженні типів ендпоінтів:', error);
     }
 }
+
 
 
 document.addEventListener("DOMContentLoaded", function () {
